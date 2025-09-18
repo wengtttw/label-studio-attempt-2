68c6c7fd86736d0b90e7d310cfae36984a3c2ef6
wengtttw <52143518+wengtttw@users.noreply.github.com>
2025-09-18 17:34:21 +0800

    Added showing or user roles in organization page
    Will work on the role assignment for owner and admin

--- web/apps/labelstudio/src/pages/Organization/PeoplePage/SelectedUser.jsx
+++ web/apps/labelstudio/src/pages/Organization/PeoplePage/SelectedUser.jsx
@@ -3,7 +3,10 @@ import { NavLink } from "react-router-dom";
 import { IconCross } from "@humansignal/icons";
 import { Userpic, Button } from "@humansignal/ui";
 import { Block, Elem } from "../../../utils/bem";
+import { useQuery } from "@tanstack/react-query";
+import { API } from "apps/labelstudio/src/providers/ApiProvider";
 import "./SelectedUser.scss";
+import { useState } from "react";
 
 const UserProjectsLinks = ({ projects }) => {
   return (
@@ -23,11 +26,35 @@ const UserProjectsLinks = ({ projects }) => {
   );
 };
 
+const ROLE_OPTIONS = [
+  { value: "admin", label: "Administrator" },
+  { value: "annotator", label: "Annotator" },
+  { value: "reviewer", label: "Reviewer" },
+  { value: "inactive", label: "Inactive" },
+];
+
+
 export const SelectedUser = ({ user, onClose }) => {
   const fullName = [user.first_name, user.last_name]
     .filter((n) => !!n)
     .join(" ")
     .trim();
+
+  //Fetch organization member info for this user
+  const { data: orgMember, error, isLoading } = useQuery({
+    queryKey: [user?.active_organization, user?.id, "user-membership"],
+    queryFn: async () => {
+      if (!user?.active_organization  || !user.id) return null;
+      return await API.invoke("userMemberships", {
+        pk: user.active_organization,
+        userPk: user.id,
+      });
+    },
+    enabled: !!user?.active_organization && !!user?.id,
+  });
+
+  console.log("orgMember data:", orgMember); // Debug orgMember data
+  if (error) console.error("Error fetching orgMember:", error);
 
   return (
     <Block name="user-info">
@@ -47,6 +74,16 @@ export const SelectedUser = ({ user, onClose }) => {
           <Elem tag="p" name="email">
             {user.email}
           </Elem>
+          {orgMember?.role && (
+            <Elem tag="p" name="role">
+              Role: {orgMember.role}
+            </Elem>
+          )}
+          {!isLoading && !orgMember?.role && (
+            <Elem tag="p" name="role">
+              Role not found
+            </Elem>
+          )}
         </Elem>
       </Elem>
 
